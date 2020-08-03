d4c3d6eb77eb3b6eb509908cd3ffd7dab754b925
AwinZhang315 <2298846564@qq.com>
2020-08-03 21:41:13 +0800

    init the project

--- vhrserver/vhr-web/src/main/java/org/awin/vhr/VhrApplication.java
+++ vhrserver/vhr-web/src/main/java/org/awin/vhr/VhrApplication.java
@@ -1,2 +1,15 @@
-package org.awin.vhr;public class VhrApplication {
+package org.awin.vhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@EnableScheduling
+public class VhrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VhrApplication.class,args);
+    }
 }
